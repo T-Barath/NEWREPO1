91166a3b4faac926fe50793ce3a5d7a0b979d4cd
barath <105115024+T-Barath@users.noreply.github.com>
2022-08-24 10:49:55 +0530

    Update Interface11.java

--- src/com/AbstractClasss/Interface11.java
+++ src/com/AbstractClasss/Interface11.java
@@ -2,19 +2,7 @@ package com.AbstractClasss;
 
 public class Interface11 {
 private void studentId(int num) {
-}
-private void studentId(String name) { \\ depends on order
-}
-private void studentId(String email, int ph) { \\depends on data 
-type
-}
-private void studentId(int dob, String add) { \\depends on 
-datatype count
-}
-public static void main(String[] arg) { 
-StudentInfo info = new StudentInfo();
-}
-}
 
+}
 
 }
